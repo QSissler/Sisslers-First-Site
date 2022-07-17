5a85b9d1fe4370545e90fac38727ed32ad04298f
Quinn Sissler <QSissler@gmail.com>
2022-07-17 17:56:55 -0400

    add eventlistener to script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//const h2 = document.createElement("h2");
-//h2.textContent = "This content added by JavaScript";
-
-//document.querySelector("body").appendChild(h2);
+const input = document.getElementById("image");
+input.addEventListener("click", function(){
+    input.classList.toggle("img-rounded-border");
+})
